39dffa6f6cf91fd12dc26474c34b7b8bb2e05bc8
Tatsunori Morita <tatsunori.morita@gmail.com>
2022-05-27 15:55:44 +0900

    Changed task data class.

--- tasks_app_ios/Model/Task.swift
+++ tasks_app_ios/Model/Task.swift
@@ -5,20 +5,46 @@
 //  Created by Tatsunori on 2022/04/14.
 //
 
-struct Task: Codable {
-    private var _text: String
-    private var _isChecked: Bool
+import Foundation
 
-    init(text: String, isChecked: Bool) {
-        _text = text
+struct Task: Codable {
+    private let _id: String
+    private let _title: String
+    private let _notes: String
+    private let _isChecked: Bool
+    private let _parentId: String
+    private let _order: Double
+
+    init(id: String = "", title: String, notes: String = "", isChecked: Bool, parentId: String = "", order: Double = 0.0) {
+        _id = id.isEmpty ? UUID().uuidString : id
+        _title = title
+        _notes = notes
         _isChecked = isChecked
+        _parentId = parentId
+        _order = order
     }
 
-    public var getText: String {
-        _text
+    public var getId: String {
+        _id
+    }
+
+    public var getTitle: String {
+        _title
+    }
+
+    public var getNotes: String {
+        _notes
     }
 
     public var getIsChecked: Bool {
         _isChecked
+    }
+
+    public var getParentId: String {
+        _parentId
+    }
+
+    public var getOrder: Double {
+        _order
     }
 }
